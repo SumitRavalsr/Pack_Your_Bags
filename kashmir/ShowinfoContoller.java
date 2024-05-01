2dc36a1510d73b5cd6cc8a7577c3efc58f01cab2
Sumit Raval <158709364+SumitRavalsr@users.noreply.github.com>
2024-05-01 20:49:45 +0530

    Add files via upload

---
+++ kashmir/ShowinfoContoller.java
@@ -0,0 +1,28 @@
+package kashmir;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ShowinfoContoller {
+
+public void switchscene(ActionEvent event) throws IOException {
+
+	Parent root=FXMLLoader.load(getClass().getResource("/application/Dashboard.fxml"));
+	
+	Stage stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+
+	Scene scene = new Scene(root,1540,815);
+
+	scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+
+	stage.setScene(scene);
+	stage.show();
+}
+
+}
